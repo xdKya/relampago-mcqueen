e536eee501ff5308fb67f549def001b1563c4119
xdKya <90745676+xdKya@users.noreply.github.com>
2022-07-12 15:26:56 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# relampago-mcqueen
+# C36RV_SpeedRacer_Reference_Code
+Teacher Reference Code
---
+++ js/Form.js
@@ -0,0 +1,39 @@
+class Form {
+  constructor() {
+    this.input = createInput("").attribute("placeholder", "Digite seu nome");
+    this.playButton = createButton("Jogar");
+    this.titleImg = createImg("./assets/TITULO.png", "nome do jogo");
+    this.greeting = createElement("h2");
+  }
+
+  hide() {
+    this.greeting.hide();
+    this.playButton.hide();
+    this.input.hide();
+
+
+  }
+
+  showElements(){
+    this.input.position(width/2-100,height/2);
+    this.playButton.position(width/2-80,height/2 + 50);
+    this.titleImg.position(120,50);
+    this.greeting.position(width/2,height/2);
+
+    this.titleImg.class("gameTitle");
+    this.input.class("customInput");
+    this.playButton.class("customButton");
+    this.greeting.class("greeting");
+  }
+
+  mousePressed(){
+
+  }
+  
+  display(){
+    this.showElements();
+    this.mousePressed();
+  }
+}
+
+ 
